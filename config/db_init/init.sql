952839e2a1500525944517cee5180a026cf79be5
mrmuffinman <harisoncom@gmail.com>
2025-07-14 15:32:34 +0100

    Minor data addition
    Adding hashed password column for accounts and adding temporary account for testing

--- config/db_init/init.sql
+++ config/db_init/init.sql
@@ -4,10 +4,11 @@ CREATE TABLE
         id SERIAL PRIMARY KEY,
         username VARCHAR(50) NOT NULL,
         email VARCHAR(100) NOT NULL,
+        password VARCHAR(100) NOT NULL, -- BCRYPT to be used as standard
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
 INSERT INTO
-    users (username, email)
+    users (username, email, password)
 VALUES
-    ('muffin', 'muffin@example.com');
+    ('muffin', 'muffin@example.com','$2b$12$oQMlODBxoigy54K5dlMohel805jNFzuFgTKkoklUKukxz4A6mAipC');
